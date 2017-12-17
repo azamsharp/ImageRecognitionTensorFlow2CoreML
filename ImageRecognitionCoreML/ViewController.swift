4870e0786ac236f7828ce7d87a07176b4c6a3ef4
Mohammad Azam <azamsharp@Mohammads-MacBook-Pro.local>
2017-12-17 09:17:20 -0600

    first commit

--- ImageRecognitionCoreML/ViewController.swift
+++ ImageRecognitionCoreML/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pictureImageView :UIImageView!
+    @IBOutlet weak var titleLabel :UILabel!
+    
+    private var model :my_model = my_model()
+    
+    let images = ["cat.jpg","dog.jpg","rat.jpg","banana.jpg"]
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func nextButtonPressed() {
+        
+        if index > self.images.count - 1 {
+            index = 0
+        }
+        
+        let img = UIImage(named: images[index])
+        self.pictureImageView.image = img
+        
+        let resizedImage = img?.resizeTo(size: CGSize(width: 224, height: 224))
+        
+        let buffer = resizedImage?.toBuffer()
+        
+        let prediction = try! self.model.prediction(input: my_modelInput(input__0: buffer!))
+        
+        //let prediction = try! self.model.prediction(image: buffer!)
+        
+        self.titleLabel.text = prediction.classLabel
+        
+        index = index + 1
     }
-
 
 }
 
